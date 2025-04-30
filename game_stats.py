37692fa75c54f9db97bd97e442a1612a580f7082
Gabi Melby <melbyg@huntington.edu>
2025-04-30 08:32:39 -0400

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,4 +1,16 @@
+# Author: Gabi Melby
+#
+# Date: 4/30/2025
+#
+# Description: This is the Model file for the MVC. This is 
+# the data and logic that tracks the kills, stops, gamestate,
+# and also manages game history.
+#
+#Used ChatGPT and DeepSeek. 
+# -------------------------------------------------------
+
 class GameStats:
+    #sets the default values as 0
     def __init__(self):
         self.q1_kills = 0
         self.q2_kills = 0
@@ -8,17 +20,17 @@ class GameStats:
         self.is_kill_active = False
         self.consecutive_stops = 0
 
-    @property
+    @property #calculates the total amount of kills in all quarters
     def total_kills(self):
         return self.q1_kills + self.q2_kills + self.q3_kills + self.q4_kills
 
-    def add_stop(self):
-        self.total_stops += 1
-        self.consecutive_stops += 1
-        if self.consecutive_stops >= 3:
-            self.is_kill_active = True
+    def add_stop(self): #this is the counter to add stops
+        self.total_stops += 1 #total stops throughout the whole game
+        self.consecutive_stops += 1 #stops in a row cann be reset
+        if self.consecutive_stops >= 3: #tracks if opportunity for kill
+            self.is_kill_active = True #activates the kill
 
-    def add_kill(self, quarter):
+    def add_kill(self, quarter): #adds the kills to the quarter
         if not self.is_kill_active:
             return
         
@@ -32,22 +44,22 @@ class GameStats:
             self.q4_kills += 1
             
         self.is_kill_active = False
-        self.consecutive_stops = 0
+        self.consecutive_stops = 0 #resets stops in a row if someone scored
 
     def reset_stops(self):
-        self.is_kill_active = False
+        self.is_kill_active = False 
         self.consecutive_stops = 0
 
-    def reset_all(self):
-        self.q1_kills = 0
+    def reset_all(self):  #this resets the whole game to default values
+        self.q1_kills = 0 
         self.q2_kills = 0
         self.q3_kills = 0
         self.q4_kills = 0
         self.reset_stops()
 
 
-class SavedGame:
+class SavedGame:  #gives the opponent, date, and game stats for saved game
     def __init__(self, opponent, date, stats):
-        self.opponent = opponent
-        self.date = date
-        self.stats = stats
+        self.opponent = opponent #string
+        self.date = date #date
+        self.stats = stats #game stats object
