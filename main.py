a6fc9a00cdc21fef9d329d22bcf7c57c57ce38d1
Gabi Melby <melbyg@huntington.edu>
2025-04-30 08:30:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+# Author: Gabi Melby
+#
+# Date: 4/30/2025
+#
+# Description: Create an app that tracks the amount
+# of kills in a basketball game and saves as a json file.
+#
+#Used ChatGPT, DeepSeek, GeeksForGeeks, and class assignments. 
+# -------------------------------------------------------
+
+
 import tkinter as tk
 from kill_tracker_controller import KillTrackerController
-from kill_tracker_view import KillTrackerView, ConsoleView
+from kill_tracker_view import KillTrackerView 
 
-def run_gui():
+def run_gui(): #runs the actual app 
     root = tk.Tk()
     controller = KillTrackerController()
     app = KillTrackerView(root, controller)
     root.mainloop()
 
-def run_console():
-    controller = KillTrackerController()
-    view = ConsoleView(controller)
-    view.run()
+ 
 
 if __name__ == "__main__":
     # To run the GUI version:
     run_gui()
-    
-    # To run the console version instead:
-    # run_console()
